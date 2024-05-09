60f496f74f94aebfc22e94d05e13687f3a692dda
Julia Wysocka <268329@student.pwr.edu.pl>
2024-05-09 10:26:33 +0200

    update

---
+++ src/loginForm/LoginForm.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useMemo } from 'react';
+import './LoginForm.css';
+import { Formik } from 'formik';
+import { Button, TextField } from '@mui/material';
+import * as yup from 'yup';
+
+function LoginForm() {
+  const onSubmit = useCallback(
+    (values: { username: string; password: string }, formik: any) => {
+      console.log(values);
+    },
+    [],
+  );
+
+  const validationSchema = useMemo(
+    () =>
+      yup.object().shape({
+        username: yup.string().required('Username is required'),
+        password: yup
+          .string()
+          .required('Password is required')
+          .min(5, 'Password is too short'),
+      }),
+    [],
+  );
+
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+      validateOnChange
+      validateOnBlur
+    >
+      {(formik: any) => (
+        <form
+          className="loginForm"
+          id="signFrom"
+          onSubmit={formik.handleSubmit}
+          noValidate
+        >
+          <TextField
+            id="username"
+            label="username"
+            variant="standard"
+            name="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.username && formik.errors.username}
+            helperText={formik.touched.username && formik.errors.username}
+          />
+          <TextField
+            id="password"
+            type="password"
+            label="password"
+            variant="standard"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && formik.errors.password}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            form="signForm"
+            disabled={!(formik.isValid && formik.dirty)}
+          >
+            Zaloguj się
+          </Button>
+        </form>
+      )}
+    </Formik>
+  );
+}
+
+export default LoginForm;
